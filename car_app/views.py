29dbb7aa5da878458bac0f81c56e5903bd3b3eb1
Davit Lomauridze <davit13.lomauridze@gmail.com>
2025-12-05 11:23:16 +0400

    [FIX] small adjustments

--- car_app/views.py
+++ car_app/views.py
@@ -5,6 +5,7 @@ from pyexpat import model
 from typing import Any
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Car
 # Create your views here.
 
 @dataclass
@@ -22,12 +23,14 @@ class SearchTerm:
 
 #წამოიღოს ყველა
 def get_all(request):
+    data = Car.objects.all()
     pass
 #წამოიღოს კონკრეტული მანქანა ID-ით
 def get(request, car_id):
     pass
 
 #წამოიღოს კონკრეტლი მწარმოებელის მანქანები
+#api/v1/cars/make/<make:str>/
 def get_by_make(request, make):
     pass
 
@@ -37,7 +40,7 @@ def get_by_price_range(request, min_price, max_price):
 
 #წამოიღოს მანქანების მოდელების სახელები (მხოლოდ სახელები ტუპლი არა) რაღაც ფასსზე ნაკლები
 #ან (ფასი გაყოფილი 500) ზე stock ზე ნაკლები
-def get_by_price_less_or_stock_less_then_price(request, search_term:SearchTerm):
+def get_by_price_less_or_stock_less_then_price(request, price, stock):
     pass
 
 #ეს მერე ვქნათ
